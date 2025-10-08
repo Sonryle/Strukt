c350b07400442bb9e84a19e1a35ee19dd412aa58
Joseppius <cathcartj08@gmail.com>
2025-10-08 13:00:24 +0100

    Project directory is now created if it doesnt exist

--- src/project_details.c
+++ src/project_details.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #include <project_details.h>
 
@@ -15,26 +16,34 @@ int fetch_project_details()
 
 void find_project_data_dir()
 {
-        /* Find project data dir */
 #if defined(_WIN32)
+        /* Find project data dir */
         const char* home = getenv("APPDATA");
         const char* rest = "\\";
-#else
-        const char* home = getenv("HOME");
-        const char* rest = "/.local/share/";
-#endif
         project_data_dir = calloc(1, sizeof(char) * (strlen(home) + strlen(rest) + strlen(PROJECT_NAME) + 2));
         if (project_data_dir != NULL)
         {
                 strcat(project_data_dir, home);
                 strcat(project_data_dir, rest);
                 strcat(project_data_dir, PROJECT_NAME);
-#if defined(_WIN32)
                 strcat(project_data_dir, "\\");
-#else
-                strcat(project_data_dir, "/");
-#endif
         }
+        
+        mkdir(project_data_dir, 0777);
+#else
+        const char* home = getenv("HOME");
+        const char* rest = "/.local/share/";
+        project_data_dir = calloc(1, sizeof(char) * (strlen(home) + strlen(rest) + strlen(PROJECT_NAME) + 2));
+        if (project_data_dir != NULL)
+        {
+                strcat(project_data_dir, home);
+                strcat(project_data_dir, rest);
+                strcat(project_data_dir, PROJECT_NAME);
+                strcat(project_data_dir, "/");
+        }
+
+        mkdir(project_data_dir, 0777);
+#endif
 }
 
 void terminate_project_details()
